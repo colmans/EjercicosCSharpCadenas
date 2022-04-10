d95ca8de6c09290b42ac2d2b803b281d69524ea5
Sabrina Colman <sabrina-rct@hotmail.com>
2022-04-10 17:41:54 -0300

    Agregar archivos de proyecto.

---
+++ EjercicosCSharpCadenas/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*7- Solicite al usuario ingrese su nombre y presente el mensaje "Hola [nombre]"*/
+Console.WriteLine("Por favor ingrese su nombre");
+string nombre = Console.ReadLine();
+Console.WriteLine("Hola" + " " + nombre);   
+
+/*8- 
+ */
