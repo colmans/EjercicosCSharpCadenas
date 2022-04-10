ad801a81b12c20e61bcc4ca0dd2027208ae0c13a
Sabrina Colman <sabrina-rct@hotmail.com>
2022-04-10 18:31:44 -0300

    Mayuscula

--- EjercicosCSharpCadenas/Program.cs
+++ EjercicosCSharpCadenas/Program.cs
@@ -3,7 +3,19 @@
 /*7- Solicite al usuario ingrese su nombre y presente el mensaje "Hola [nombre]"*/
 Console.WriteLine("Por favor ingrese su nombre");
 string nombre = Console.ReadLine();
-Console.WriteLine("Hola" + " " + nombre);   
+Console.WriteLine("Hola" + " " + nombre);
 
-/*8- 
+/*8-
  */
+
+Console.WriteLine("Por favor ingrese una Frase");
+string frase = Console.ReadLine();
+Console.WriteLine("Tiene la palabra fin?   ");
+
+/*9-
+ * mostrarlo en mayuscula: 
+ */
+
+Console.WriteLine("Ingrese una palabra cualquiera");
+string fraseEnmayuscula = Console.ReadLine();
+Console.WriteLine(fraseEnmayuscula.ToLowerInvariant);
